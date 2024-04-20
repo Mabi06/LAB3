bc0b29c0e6c0810bfbcd262bd844866eceeee75b
Mabi06 <161559444+Mabi06@users.noreply.github.com>
2024-04-20 17:22:56 +0800

    Update Register_index.php

--- web/Register_index.php
+++ web/Register_index.php
@@ -21,13 +21,13 @@ function sendMail($email, $v_code){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'mangaronarch@gmail.com';                     //SMTP username
-        $mail->Password   = 'atdw cugc zqnz iadm';                               //SMTP password
+        $mail->Username   = 'zapantamaribel06.com';                     //SMTP username
+        $mail->Password   = 'watapampic';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
         //Recipients
-        $mail->setFrom('mangaronarch@gmail.com', 'validation');
+        $mail->setFrom('zapantamaribel06.com', 'validation');
         $mail->addAddress($email);
 
     
